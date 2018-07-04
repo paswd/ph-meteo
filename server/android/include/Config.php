6c3d9a62de4087f4f431d59485366bc69da335ec
paswd <paswd@yandex.ru>
2018-07-04 14:20:23 +0300

    Added scripts for android app

---
+++ server/android/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "phmeteo_bot");
+define("DB_PASSWORD", "8cNER8LekWQqZtQT");
+define("DB_DATABASE", "phmeteo");
+?>
---
+++ server/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Test</title>
+</head>
+<body>
+<form method="post" action="android/login.php">
+	<input name="email" value="" placeholder="Email">
+	<input type="password" name="password" value="" placeholder="Password">
+	<input type="submit" value="Login">
+</form>
+</form>
+</body>
+</html>
