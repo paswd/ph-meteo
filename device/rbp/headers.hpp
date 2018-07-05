8e16c49bdf657bfaa06d03f6f5cc91f96028da60
paswd <paswd@yandex.ru>
2018-07-05 17:32:35 +0300

    Added device init

---
+++ device/rbp/headers.hpp
@@ -0,0 +1,32 @@
+#ifndef _HEADERS_HPP_
+#define _HEADERS_HPP_
+
+const size_t TEMP_ARR_LEN = 3;
+const size_t PRESS_ARR_LEN = 6;
+const size_t ALT_ARR_LEN = 4;
+const size_t HUM_ARR_LEN = 3;
+const size_t INPUT_MESSAGE_SIZE =
+	TEMP_ARR_LEN +
+	PRESS_ARR_LEN +
+	ALT_ARR_LEN +
+	HUM_ARR_LEN + 1;
+
+const std::string COLOR_RED("\033[0;31m");
+const std::string COLOR_GREEN("\033[1;32m");
+const std::string COLOR_YELLOW("\033[1;33m");
+const std::string COLOR_CYAN("\033[0;36m");
+const std::string COLOR_MAGENTA("\033[0;35m");
+const std::string COLOR_RESET("\033[0m");
+
+const std::string MESSAGE_FATAL_ERROR = COLOR_RED + "FATAL ERROR! The server was interrupt" + COLOR_RESET;
+
+class RuntimeParams {
+public:
+	std::map <std::string, std::string> AppParams;
+	size_t CurrenTimeoutMinutes;
+	int Arduino;
+};
+
+RuntimeParams PARAMS;
+
+#endif
--- device/rbp/main.cpp
+++ device/rbp/main.cpp
@@ -3,32 +3,20 @@
 #include <map>
 #include <ctime>
 #include <string>
+#include <stringstream>
 #include <string.h>
 #include <unistd.h>
 #include <wiringSerial.h>
+#include "headers.hpp"
 
 using namespace std;
 
-const size_t TEMP_ARR_LEN = 3;
-const size_t PRESS_ARR_LEN = 6;
-const size_t ALT_ARR_LEN = 4;
-const size_t HUM_ARR_LEN = 3;
-const size_t INPUT_MESSAGE_SIZE =
-	TEMP_ARR_LEN +
-	PRESS_ARR_LEN +
-	ALT_ARR_LEN +
-	HUM_ARR_LEN + 1;
-
-const string COLOR_RED("\033[0;31m");
-const string COLOR_GREEN("\033[1;32m");
-const string COLOR_YELLOW("\033[1;33m");
-const string COLOR_CYAN("\033[0;36m");
-const string COLOR_MAGENTA("\033[0;35m");
-const string COLOR_RESET("\033[0m");
-
-const string MESSAGE_FATAL_ERROR = COLOR_RED + "FATAL ERROR! The server was interrupt" + COLOR_RESET;
-
-map <string, string> APP_PARAMS;
+long long StringToNum(string str) {
+	stringstream ssream(str);
+	long long res;
+	sstream >> res;
+	return res;
+}
 
 bool ReadParams(void) {
 	ifstream fin("init.conf");
@@ -42,10 +30,11 @@ bool ReadParams(void) {
 		if (is_first_param) {
 			name_tmp = tmp;
 		} else {
-			APP_PARAMS.insert(pair <string, string>(name_tmp, tmp));
+			PARAMS.AppParams.insert(pair <string, string>(name_tmp, tmp));
 		}
 		is_first_param = !is_first_param;
 	}
+	PARAMS.CurrentTimeoutMinutes = (size_t) StringToNum(PARAMS.AppParams["TIMEOUT_DEFAULT_MINUTES"]);
 	return true;
 }
 void PrintParams(void) {
@@ -54,41 +43,47 @@ void PrintParams(void) {
 	}
 }
 
-int main(void) {
+bool StartServer(void) {
 	cout << "Reading config... ";
 	if (ReadParams()) {
 		cout << COLOR_GREEN << "[OK]" << COLOR_RESET << endl;
 	} else {
 		cout << COLOR_RED << "[FAILED]" << COLOR_RESET << endl;
-		cout << MESSAGE_FATAL_ERROR << endl;
-		return 0;
+		return false;
 	}
-	PrintParams();
+	//PrintParams();
 
-	cout << "Arduino connection initialization" << endl;
+	cout << "Finding arduino" << endl;
 	string base = "/dev/ttyACM";
-	int fd = -1;
-	for (int i = 0; fd == -1 && i < 10; i++) {
+	PARAMS.Arduino = -1;
+	for (int i = 0; PARAMS.Arduino == -1 && i < 10; i++) {
 		char tmp = i + '0';
 		string addr = base + tmp;
 		cout << "Connecting " << addr << "... ";
-		fd = serialOpen(addr.c_str(), 9600);
-		if (fd != -1) {
+		PARAMS.Arduino = serialOpen(addr.c_str(), 9600);
+		if (PARAMS.Arduino != -1) {
 			cout << COLOR_GREEN << "[OK]" << COLOR_RESET;
 		} else {
 			cout << COLOR_RED << "[FAILED]" << COLOR_RESET;
 		}
 		cout << endl;
 	}
-	if (fd == -1) {
+	if (PARAMS.Arduino == -1) {
 		cout << COLOR_RED << "Arduino connection error" << COLOR_RESET << endl;
-		cout << MESSAGE_FATAL_ERROR << endl;
 
-		return 0;
+		return false;
 	}
 	cout << COLOR_GREEN << "Arduino has been successfuly found" << COLOR_RESET << endl;
+	return true;
+}
+
+int main(void) {
 	//char bf[INPUT_MESSAGE_SIZE + 1];
 	//bf[INPUT_MESSAGE_SIZE] = '\n';
+	if (!StartServer()) {
+		cout << MESSAGE_FATAL_ERROR << endl;
+		return 0;
+	}
 
 	return 0;
 }
