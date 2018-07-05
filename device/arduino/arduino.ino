63c168766bb3dadf9ebf2a28b083cc12262953c0
paswd <paswd@yandex.ru>
2018-07-05 14:07:50 +0300

    Ready total device reading data

--- device/arduino/arduino.ino
+++ device/arduino/arduino.ino
@@ -2,31 +2,36 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <DHT.h>
 
 #define BMP_SCK 13
 #define BMP_MISO 12
 #define BMP_MOSI 11 
 #define BMP_CS 10
 
-#define DHTPIN 2
+#define DHTPIN A0
 
 //Adafruit_BMP280 bmp; // I2C
 Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
-DHT dht(DHTPIN, DHT22);
+//DHT dht(DHTPIN, DHT22);
+float humVal = 0; 
+
+float calc_hum(float val){ 
+  float H; 
+  H = 0.163 * val;
+  //RH10 = val + 6*val/10 + 3*val/100; = 0.163*val 
+  return(H); 
+} 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
+  //Serial.println(F("BMP280 test"));
   
   if (!bmp.begin()) {  
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
-
-  dht.begin();
 }
 
 void loop() {
@@ -41,21 +46,12 @@ void loop() {
     Serial.print(F("Approx altitude = "));
     Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
     Serial.println(" m");
-    float h = dht.readHumidity();
 
-// Считываем температуру
-
-// Проверка удачно прошло ли считывание.
-
-    if (!isnan(h)) {
-
-        Serial.print(F("Humidity = "));
-        Serial.println(h);        
-
-    } else {
-        Serial.println("Не удается считать показания влажности");
-    }
-    
+    humVal = calc_hum(analogRead(DHTPIN));
+    Serial.print(F("Humedad = "));
+    Serial.print(humVal);
+    Serial.println(" %");
     Serial.println();
+    
     delay(2000);
 }
