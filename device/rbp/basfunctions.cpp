4f0cdb34fd3189f3f927241d542b84f5cd612979
paswd <paswd@yandex.ru>
2018-07-07 04:51:50 +0300

    Trying fixing bug in var type conflict 2

--- device/rbp/basfunctions.cpp
+++ device/rbp/basfunctions.cpp
@@ -16,6 +16,11 @@ long long StringToNum(string str) {
 string sha1_hash(string str) {
 	unsigned char hash[SHA1_HASH_LEN];
 	SHA1(reinterpret_cast<const unsigned char *>(str.c_str()), str.size() - 1, hash);
-	return string(hash);
+	//return string(hash);
+	string res = "";
+	for (size_t i = 0; i < SHA1_HASH_LEN, i++) {
+		res += hash[i];
+	}
+	return res;
 }
 
