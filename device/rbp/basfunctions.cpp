cffa8ad7efd2f8079bd68988c7722d26a337c380
paswd <paswd@yandex.ru>
2018-07-07 05:22:58 +0300

    Trying fixing bug with sha1

--- device/rbp/basfunctions.cpp
+++ device/rbp/basfunctions.cpp
@@ -13,13 +13,20 @@ long long StringToNum(string str) {
 	return res;
 }
 
+char CharToHex(unsigned char ch) {
+	if (ch <= 9) {
+		return ch + '0'; 
+	}
+	return ch + 'a' - 10;
+}
+
 string sha1_hash(string str) {
 	unsigned char hash[SHA1_HASH_LEN];
 	SHA1(reinterpret_cast<const unsigned char *>(str.c_str()), str.size() - 1, hash);
 	//return string(hash);
 	string res = "";
 	for (size_t i = 0; i < SHA1_HASH_LEN; i++) {
-		res += hash[i];
+		res += CharToHex(hash[i]);
 	}
 	return res;
 }
