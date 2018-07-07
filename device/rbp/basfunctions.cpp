90cea156f62f0981190f0dc8951c7329bbd18fd2
paswd <paswd@yandex.ru>
2018-07-08 01:23:42 +0300

    Corrected syntax

--- device/rbp/basfunctions.cpp
+++ device/rbp/basfunctions.cpp
@@ -38,39 +38,35 @@ string NumToString(long long num) {
 }
 
 long long DoubleToNum(double val) {
-  long long res = (long long) val;
-  long long md = (long long) (val * 10);
-  md %= 10;
-  if (md >= 5) {
-    res++;
-  }
-  return res;
+	long long res = (long long) val;
+	long long md = (long long) (val * 10);
+	md %= 10;
+	if (md >= 5) {
+		res++;
+	}
+	return res;
 }
 
 string sha1_hash(string str) {
 	unsigned char primary_hash[SHA1_HASH_LEN];
-	//cout << str << endl;
-	//const char *str_tmp = str.c_str();
 	unsigned char *str_tmp = new unsigned char[str.size() + 1];
+	
 	for (size_t i = 0; i < str.size(); i++) {
 		str_tmp[i] = str[i];
-		//cout << (int) str[i] << ":" << str[i] << endl;
 	}
 	str_tmp[str.size()] = 0;
 	SHA1((unsigned char *)str_tmp, str.size(), primary_hash);
 	delete [] str_tmp;
-	//return string(hash);
 	char hash[SHA1_HASH_LEN*2+1];
-	for(int i = 0; i < SHA_DIGEST_LENGTH; i++) {
-    	sprintf(&hash[i*2], "%02x", (unsigned int)primary_hash[i]);
+	
+	for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+		sprintf(&hash[i*2], "%02x", (unsigned int)primary_hash[i]);
 	}
-	//cout << hash << endl;
 	string res = "";
+	
 	for (size_t i = 0; i < SHA1_HASH_LEN; i++) {
-		//cout << (char) hash[i] << ":" << (int) hash[i] << " ";
 		res += (char) hash[i];
 	}
-	//cout << endl;
 	return res;
 }
 
